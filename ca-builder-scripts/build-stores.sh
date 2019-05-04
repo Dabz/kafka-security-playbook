0afb0ae7f3bf030e1246e8b4c853048649fd4694
Pere Urbon <pere@confluent.io>
2019-05-04 09:01:27 +0200

    parametrise the ca root dir, default password and root and intermediate CA paths during production of stores and read of certs

--- ca-builder-scripts/build-stores.sh
+++ ca-builder-scripts/build-stores.sh
@@ -1,49 +1,60 @@
 #!/usr/bin/env bash
 
-IN_KEY="ca/intermediate/private/broker1.key.pem"
-IN_CERT="ca/intermediate/certs/broker1.cert.pem"
+DEFAULT_PASSWORD=${1:-confluent}
 
-CA_CERT="ca/certs/ca.cert.pem"
-INT_CA_CERT="ca/intermediate/certs/intermediate.cert.pem"
+if [ -z "${CA_ROOT_DIR+x}" ];
+then
+CA_ROOT_DIR='.'
+fi
+
+CA_CERT="$CA_ROOT_DIR/ca/certs/ca.cert.pem"
+CA_KEY="$CA_ROOT_DIR/ca/private/ca.key.pem"
+
+INT_CA_CERT="$CA_ROOT_DIR/ca/intermediate/certs/intermediate.cert.pem"
+INT_CA_KEY="$CA_ROOT_DIR/ca/intermediate/private/intermediate.key.pem"
 
 mkdir stores tmp-certs
 
 ## buildind stores for the brokers
 for i in kafka.confluent.local
 do
-openssl pkcs12 -export -in "ca/intermediate/certs/$i.cert.pem" -inkey "ca/intermediate/private/$i.key.pem" -passin pass:confluent -passout pass:confluent -name $i -out tmp-certs/$i.p12
+CERT_PATH="$CA_ROOT_DIR/ca/intermediate/certs/$i.cert.pem"
+KEY_PATH="$CA_ROOT_DIR/ca/intermediate/private/$i.key.pem"
+openssl pkcs12 -export -in $CERT_PATH -inkey $KEY_PATH -passin pass:$DEFAULT_PASSWORD -passout pass:$DEFAULT_PASSWORD -name $i -out tmp-certs/$i.p12
 sleep 1
 ## build keystore and truststores
-keytool -noprompt -importkeystore -deststorepass confluent -destkeystore stores/broker.keystore -srckeystore tmp-certs/$i.p12 -srcstorepass confluent -storepass confluent -keypass confluent -srcstoretype PKCS12 -deststoretype pkcs12
+keytool -noprompt -importkeystore -deststorepass $DEFAULT_PASSWORD -destkeystore stores/broker.keystore -srckeystore tmp-certs/$i.p12 -srcstorepass $DEFAULT_PASSWORD -storepass $DEFAULT_PASSWORD -keypass $DEFAULT_PASSWORD -srcstoretype PKCS12 -deststoretype pkcs12
 
 done
 
-openssl pkcs12 -export -in "ca/certs/ca.cert.pem" -inkey "ca/private/ca.key.pem" -passin pass:confluent -passout pass:confluent -name "ca" -out tmp-certs/ca.p12
+openssl pkcs12 -export -in $CA_CERT -inkey $CA_KEY -passin pass:$DEFAULT_PASSWORD -passout pass:$DEFAULT_PASSWORD -name 'ca' -out tmp-certs/ca.p12
 sleep 1
-openssl pkcs12 -export -in "ca/intermediate/certs/intermediate.cert.pem" -inkey "ca/intermediate/private/intermediate.key.pem" -passin pass:confluent -passout pass:confluent -name "intermediate-ca" -out tmp-certs/inter-ca.p12
+openssl pkcs12 -export -in $INT_CA_CERT -inkey $INT_CA_KEY -passin pass:$DEFAULT_PASSWORD -passout pass:$DEFAULT_PASSWORD -name 'intermediate-ca' -out tmp-certs/inter-ca.p12
 sleep 1
 
-keytool -noprompt -importkeystore -deststorepass confluent -destkeystore stores/broker.truststore -srckeystore tmp-certs/ca.p12 -srcstorepass confluent -srcstoretype PKCS12 -storepass confluent -keypass confluent -deststoretype pkcs12
+keytool -noprompt -importkeystore -deststorepass $DEFAULT_PASSWORD -destkeystore stores/broker.truststore -srckeystore tmp-certs/ca.p12 -srcstorepass $DEFAULT_PASSWORD -srcstoretype PKCS12 -storepass $DEFAULT_PASSWORD -keypass $DEFAULT_PASSWORD -deststoretype pkcs12
 sleep 1
-keytool -noprompt -importkeystore -deststorepass confluent -destkeystore stores/broker.truststore -srckeystore tmp-certs/inter-ca.p12 -srcstorepass confluent -srcstoretype PKCS12 -storepass confluent -keypass confluent -deststoretype pkcs12
+keytool -noprompt -importkeystore -deststorepass $DEFAULT_PASSWORD -destkeystore stores/broker.truststore -srckeystore tmp-certs/inter-ca.p12 -srcstorepass $DEFAULT_PASSWORD -srcstoretype PKCS12 -storepass $DEFAULT_PASSWORD -keypass $DEFAULT_PASSWORD -deststoretype pkcs12
 
 
 ## building stores for the producers
 
 for i in producer2
 do
-openssl pkcs12 -export -in "ca/intermediate/certs/$i.cert.pem" -inkey "ca/intermediate/private/$i.key.pem" -passin pass:confluent -passout pass:confluent -name $i -out tmp-certs/$i.p12
+CERT_PATH="$CA_ROOT_DIR/ca/intermediate/certs/$i.cert.pem"
+KEY_PATH="$CA_ROOT_DIR/ca/intermediate/private/$i.key.pem"
+openssl pkcs12 -export -in $CERT_PATH -inkey $KEY_PATH -passin pass:$DEFAULT_PASSWORD -passout pass:$DEFAULT_PASSWORD -name $i -out tmp-certs/$i.p12
 sleep 1
 ## build keystore and truststores
-keytool -noprompt -importkeystore -deststorepass confluent -destkeystore stores/client.keystore -srckeystore tmp-certs/$i.p12 -srcstorepass confluent -storepass confluent -keypass confluent -srcstoretype PKCS12 -deststoretype pkcs12
+keytool -noprompt -importkeystore -deststorepass $DEFAULT_PASSWORD -destkeystore stores/client.keystore -srckeystore tmp-certs/$i.p12 -srcstorepass $DEFAULT_PASSWORD -storepass $DEFAULT_PASSWORD -keypass $DEFAULT_PASSWORD -srcstoretype PKCS12 -deststoretype pkcs12
 
 done
 
-openssl pkcs12 -export -in "ca/certs/ca.cert.pem" -inkey "ca/private/ca.key.pem" -passin pass:confluent -passout pass:confluent -name "ca" -out tmp-certs/ca.p12
+openssl pkcs12 -export -in $CA_CERT -inkey $CA_KEY -passin pass:$DEFAULT_PASSWORD -passout pass:$DEFAULT_PASSWORD -name "ca" -out tmp-certs/ca.p12
 sleep 1
-openssl pkcs12 -export -in "ca/intermediate/certs/intermediate.cert.pem" -inkey "ca/intermediate/private/intermediate.key.pem" -passin pass:confluent -passout pass:confluent -name "intermediate-ca" -out tmp-certs/inter-ca.p12
+openssl pkcs12 -export -in $INT_CA_CERT -inkey $INT_CA_KEY -passin pass:$DEFAULT_PASSWORD -passout pass:$DEFAULT_PASSWORD -name "intermediate-ca" -out tmp-certs/inter-ca.p12
 sleep 1
 
-keytool -noprompt -importkeystore -deststorepass confluent -destkeystore stores/client.truststore -srckeystore tmp-certs/ca.p12 -srcstorepass confluent -srcstoretype PKCS12 -storepass confluent -keypass confluent -deststoretype pkcs12
+keytool -noprompt -importkeystore -deststorepass $DEFAULT_PASSWORD -destkeystore stores/client.truststore -srckeystore tmp-certs/ca.p12 -srcstorepass $DEFAULT_PASSWORD -srcstoretype PKCS12 -storepass $DEFAULT_PASSWORD -keypass $DEFAULT_PASSWORD -deststoretype pkcs12
 sleep 1
-keytool -noprompt -importkeystore -deststorepass confluent -destkeystore stores/client.truststore -srckeystore tmp-certs/inter-ca.p12 -srcstorepass confluent -srcstoretype PKCS12 -storepass confluent -keypass confluent -deststoretype pkcs12
+keytool -noprompt -importkeystore -deststorepass $DEFAULT_PASSWORD -destkeystore stores/client.truststore -srckeystore tmp-certs/inter-ca.p12 -srcstorepass $DEFAULT_PASSWORD -srcstoretype PKCS12 -storepass $DEFAULT_PASSWORD -keypass $DEFAULT_PASSWORD -deststoretype pkcs12
