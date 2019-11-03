457cdb915ab444952f0e73149264721739b12817
Pere Urbon <pere@confluent.io>
2019-11-03 20:56:41 +0100

    formalize the verify script

--- kafka-connect-mtls/check-ssl-client-auth.sh
+++ kafka-connect-mtls/check-ssl-client-auth.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
-openssl pkcs12 -in connect/secrets/rest-client.p12 -out connect/secrets/ca.pem -cacerts -nokeys -passin pass:confluent -passout pass:confluent
-openssl pkcs12 -in connect/secrets/rest-client.p12 -out connect/secrets/client.pem -clcerts -nokeys -passin pass:confluent -passout pass:confluent
-openssl pkcs12 -in connect/secrets/rest-client.p12 -out connect/secrets/key.pem -nocerts -passin pass:confluent -passout pass:confluent
+verify_ssl_client_auth () {
 
-curl --insecure --key connect/secrets/key.pem --cacert connect/secrets/ca.pem --cert connect/secrets/client.pem:confluent https://localhost:18083
+  openssl pkcs12 -in connect/secrets/$1.p12 -out connect/secrets/ca.pem -cacerts -nokeys -passin pass:confluent -passout pass:confluent
+  openssl pkcs12 -in connect/secrets/$1.p12 -out connect/secrets/client.pem -clcerts -nokeys -passin pass:confluent -passout pass:confluent
+  openssl pkcs12 -in connect/secrets/$1.p12 -out connect/secrets/key.pem -nocerts -passin pass:confluent -passout pass:confluent
+
+  curl --insecure --key connect/secrets/key.pem --cacert connect/secrets/ca.pem --cert connect/secrets/client.pem:confluent https://localhost:18083
+}
+
+echo "Check SSL client auth with an unknown certificate"
+verify_ssl_client_auth "certificate"
+
+echo "Check SSL client auth with a valid client"
+verify_ssl_client_auth "rest-client"
