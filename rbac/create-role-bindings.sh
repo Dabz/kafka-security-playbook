35ceefec8d1e80c8bb76f715dd65c9c8ef153309
Kiril Piskunov <kiril@confluent.io>
2020-07-10 10:45:55 +0100

    [RBAC] Confluent CLI in a can (#59)
    * Confluent CLI in a can
    if everything is in Docker why expect to have Confluent CLI installed locally
    
    * Update up

--- rbac/create-role-bindings.sh
+++ rbac/create-role-bindings.sh
@@ -3,15 +3,15 @@
 ################################## GET KAFKA CLUSTER ID ########################
 ZK_CONTAINER=zookeeper
 ZK_PORT=2181
-echo "Retrieving Kafka cluster id from docker-container '$ZK_CONTAINER' port '$ZK_PORT'"
-KAFKA_CLUSTER_ID=$(docker exec -it $ZK_CONTAINER zookeeper-shell localhost:$ZK_PORT get /cluster/id 2> /dev/null | grep \"version\" | jq -r .id)
+
+KAFKA_CLUSTER_ID=$(curl -s http://kafka:8090/v1/metadata/id | jq -r ".id")
 if [ -z "$KAFKA_CLUSTER_ID" ]; then
     echo "Failed to retrieve kafka cluster id from zookeeper"
     exit 1
 fi
 
 ################################## SETUP VARIABLES #############################
-MDS_URL=http://localhost:8090
+MDS_URL=http://kafka:8090
 CONNECT=connect-cluster
 SR=schema-registry
 KSQL=default_
